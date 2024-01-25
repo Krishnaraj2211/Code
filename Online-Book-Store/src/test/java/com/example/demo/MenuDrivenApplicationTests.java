bd6f324eb978aafd1a8eab7a3c65bceda697c877
Krishnaraj2211 <krishnajaganathan22@gmail.com>
2024-01-25 13:49:20 +0530

    commit

---
+++ Online-Book-Store/src/test/java/com/example/demo/MenuDrivenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.online.book.store.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Main.class) // Replace MenuDrivenApplication with the name of your main application class
+public class MenuDrivenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		// Your test logic here
+	}
+}
